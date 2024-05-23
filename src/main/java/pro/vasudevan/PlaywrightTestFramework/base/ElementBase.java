29a200b09ea166aa399b526356fa7dd8aefed920
Vasudevan <vasudevan.s@outlook.com>
2024-05-23 15:44:52 -0700

    Update ElementBase.java
    fixed typo in comment

--- src/main/java/pro/vasudevan/PlaywrightTestFramework/base/ElementBase.java
+++ src/main/java/pro/vasudevan/PlaywrightTestFramework/base/ElementBase.java
@@ -15,7 +15,7 @@ Created By: Vasudevan Sampath
 
  ElementBase.java has web element specific methods.
  Also, initializes object repository file (expects object.repo.properties under src/test/resources in your test suite).
- Use getLocatorByKey() to get a Locator object for a valid object id defined in the properties file
+ Use getLocatorRefByKey() to get a Locator object for a valid object id defined in the properties file
  and use getLocatorRef() method if direct reference is needed without storing in the properties file
  */
 public class ElementBase implements IPlaywrightTestConfig {
